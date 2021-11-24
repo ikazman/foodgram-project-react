8b45f4e33a09fa4df2f67d5a86895b91116a29d3
Aleksey Petukhov <ikazman@yandex.ru>
2021-11-24 18:29:59 +0300

    PEP8 tweaks

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,8 @@
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers
-from users.serializers import UserSerializer
-
 from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
                             ShoppingCart, Tag)
+from rest_framework import serializers
+from users.serializers import UserSerializer
 
 
 class IngredientSerializer(serializers.ModelSerializer):
