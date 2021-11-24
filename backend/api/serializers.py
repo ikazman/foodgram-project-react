5e8f6bfa778ae282a0aa27f577b19bcd530f6515
Aleksey Petukhov <ikazman@yandex.ru>
2021-11-24 18:27:22 +0300

    Added ShoppingCart, Favorite, Recipe serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -18,3 +18,80 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('id', 'name', 'color', 'slug')
+
+
+class IngredientAmountSerializer(serializers.ModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(queryset=Ingredient.objects.all())
+    amount = serializers.IntegerField()
+
+    class Meta:
+        model = IngredientAmount
+        fields = ('id', 'amount')
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+
+    image = Base64ImageField()
+    author = UserSerializer(read_only=True)
+    ingredients = IngredientAmountSerializer(many=True)
+    tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(),
+                                              many=True)
+    is_in_shopping_cart = serializers.SerializerMethodField(read_only=True)
+    is_favorited = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'name',
+            'author',
+            'ingredients',
+            'image',
+            'text',
+            'cooking_time',
+            'tags',
+            'is_in_shopping_cart',
+            'is_favorited',
+        )
+
+    def get_is_in_shopping_cart(self, obj):
+        request = self.context.get('request')
+        return (request.user.is_authenticated
+                and obj.filter(user=request.user).exists())
+
+    def get_is_favorited(self, obj):
+        request = self.context.get('request')
+        return (request.user.is_authenticated
+                and obj.filter(user=request.user).exists())
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+
+    id = serializers.PrimaryKeyRelatedField(queryset=Recipe.objects.all())
+    name = serializers.SlugRelatedField(queryset=Recipe.objects.all(),
+                                        read_only=True,
+                                        slug_field='name')
+    image = Base64ImageField(read_only=True)
+    cooking_time = serializers.SlugRelatedField(queryset=Recipe.objects.all(),
+                                                read_only=True,
+                                                slug_field='cooking_time')
+
+    class Meta:
+        model = ShoppingCart
+        fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+
+    id = serializers.PrimaryKeyRelatedField(queryset=Recipe.objects.all())
+    name = serializers.SlugRelatedField(queryset=Recipe.objects.all(),
+                                        read_only=True,
+                                        slug_field='name')
+    image = Base64ImageField(read_only=True)
+    cooking_time = serializers.SlugRelatedField(queryset=Recipe.objects.all(),
+                                                read_only=True,
+                                                slug_field='cooking_time')
+
+    class Meta:
+        model = Favorite
+        fields = ('id', 'name', 'image', 'cooking_time')
