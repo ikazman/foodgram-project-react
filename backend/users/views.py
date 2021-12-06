2866b2f425d2d417d8c8905381a328c468188894
Aleksey Petukhov <ikazman@yandex.ru>
2021-12-06 18:00:28 +0400

    PEP8 tweaks, isort

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,7 @@
-from rest_framework import filters,status, viewsets
+from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.permissions import  AllowAny, IsAuthenticated
-
 
 from .models import Follow, User
 from .serializers import FollowSerializer, UserSerializer
